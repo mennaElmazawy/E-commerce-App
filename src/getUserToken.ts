4016322190ff637d26848bf14782429f13a2b712
mennaElmazawy <menahossam50@gmail.com>
2025-09-21 15:02:26 +0300

    first commit

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -5,21 +5,27 @@ import { cookies } from "next/headers";
 export async function getUserToken() {
 
 
-    const TokenSession =(process.env.Node_ENV ==="production"?'_Secure-next-auth.session-token':"next-auth.session-token")
+    const TokenSession =
+        process.env.NODE_ENV === "production"
+            ? "_Secure-next-auth.session-token"
+            : "next-auth.session-token";
     const cookiesData = await cookies()
     const encryptedToken = cookiesData.get(TokenSession)?.value
-
+    if (!encryptedToken) return null;
     const data = await decode({ token: encryptedToken, secret: process.env.NEXTAUTH_SECRET! })
-    
+
     return data?.token
 }
 
 
 export async function getUserIdFromToken() {
-    const TokenSession =(process.env.Node_ENV ==="production"?'_Secure-next-auth.session-token':"next-auth.session-token")
+    const TokenSession =
+        process.env.NODE_ENV === "production"
+            ? "_Secure-next-auth.session-token"
+            : "next-auth.session-token";
     const cookiesData = await cookies()
     const encryptedToken = cookiesData.get(TokenSession)?.value
-
+    if (!encryptedToken) return null;
     const data = await decode({ token: encryptedToken, secret: process.env.NEXTAUTH_SECRET! })
     return data?.sub
 }
